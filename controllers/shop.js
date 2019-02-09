5f51308761f43584f4233bdedd6bd0d03239778d
Arsenii Bylym <arseniibylym@gmail.com>
2019-02-09 22:56:55 +0200

    Create base project structure

---
+++ controllers/shop.js
@@ -0,0 +1,32 @@
+const Product = require('../models/product');
+
+exports.getHomePage = (req, res, next) => {
+    Product.find()
+        .then(products => {
+            console.log(products);
+            res.render(`shop/homePage`, {
+                products: products,
+                path: `/`,
+            })
+        })
+        .catch(err => {
+            const error = new Error(err);
+            error.httpStatusCode = 500;
+            return next(error);
+        });
+};
+
+exports.getProducts = (req, res, next) => {
+    Product.find()
+        .then(products => {
+            res.render(`shop/products`, {
+                products: products,
+                path: `/products`,
+            })
+        })
+        .catch(err => {
+            const error = new Error(err);
+            error.httpStatusCode = 500;
+            return next(error);
+        });
+};
---
+++ routes/shop.js
@@ -0,0 +1,11 @@
+// const path = require('path');
+const express = require('express');
+const shopController = require('../controllers/shop');
+
+const router = express.Router();
+
+router.get(`/`, shopController.getHomePage);
+router.get(`/products`, shopController.getProducts);
+router.get(`/products/:productId`, shopController.getProductItem);
+
+module.exports = router;
